eeec3048eb04a8070a2238fef8e2e10badfac617
Joel Welling <welling@psc.edu>
2020-01-15 14:36:27 -0500

    Added helloworld.py

---
+++ src/helloworld/helloworld.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+'''
+Created on Jan 15, 2020
+@author: welling
+'''
+
+from __future__ import print_function
+import six
+
+def main():
+    print("""
+    Hello World, from...
+    Joel S. Welling
+    """)
+
+if __name__ == "__main__":
+    main()
