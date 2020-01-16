cf432ca78d7c04f1eaf0dda65cd23c7e4685322d
Pankaj Bhojwani <pankaj.d.bhoj@gmail.com>
2020-01-16 14:41:40 -0500

    goodbye!

--- src/helloworld/helloworld.py
+++ src/helloworld/helloworld.py
@@ -11,7 +11,8 @@ def main():
     print("""
     Hello World, from...
     Joel S. Welling
-    and Pankaj Bhojwani
+    and Pankaj Bhojwani.
+    Goodbye World!
     """)
 
 if __name__ == "__main__":
